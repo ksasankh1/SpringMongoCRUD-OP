baa497cd82469c3284207c66c392fe3cc9815093
Bala Chadramouli, Ketharaju <ketharaju.balachadramouli@lowes.com>
2023-02-22 07:14:58 +0530

    spring mongo first commit

---
+++ src/main/java/com/mongo/springmongocrudproject/SpringmongocrudprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.springmongocrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongocrudprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongocrudprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongo/springmongocrudproject/SpringmongocrudprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.springmongocrudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongocrudprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
